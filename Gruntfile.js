18f75be923272f133927e658eb015d639d9de3c7
Boy van Amstel <boy@boyvanamstel.nl>
2015-12-23 13:31:54 +0100

    Minify and concat scripts and styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,46 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		clean: ["covervid.min.js"],
+    clean: ["covervid.min.js"],
 
-		uglify: {
-			dist: {
-				files: {
-					'covervid.min.js': ['covervid.js']
-				}
-			}
-		},
+    uglify: {
+      dist: {
+        files: {
+          'covervid.min.js': ['covervid.js'],
+          'assets/scripts/scripts.min.js': ['assets/scripts/jquery.min.js', 'covervid.js', 'assets/scripts/scripts.js']
+        }
+      }
+    },
 
-		watch: {
-			dist: {
-				files: 'covervid.js',
-				tasks: ['clean', 'uglify', 'watch']
-			}
-		}
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'assets/styles/styles.min.css': ['assets/styles/foundation.min.css', 'assets/styles/styles.css']
+        }
+      }
+    },
 
-	});
+    watch: {
+      dist: {
+        files: ['covervid.js', 'assets/**/*'],
+        tasks: ['clean', 'uglify', 'cssmin', 'watch']
+      }
+    }
 
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  });
 
-	grunt.registerTask('default', ['clean', 'uglify', 'watch']);
-	grunt.registerTask('publish', ['clean', 'uglify']);
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['clean', 'uglify', 'cssmin', 'watch']);
+  grunt.registerTask('publish', ['clean', 'uglify', 'cssmin']);
 
 };
