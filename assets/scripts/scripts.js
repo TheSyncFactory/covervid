082dd16550678feab0ae7db6fb67a87ab1a1656b
Boy van Amstel <boy@boyvanamstel.nl>
2015-12-23 11:44:14 +0100

    Try without setting width

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -7,9 +7,7 @@ $(window).scroll(headerParallax);
 function fullscreen() {
   var masthead = $('.masthead');
   var windowH = $(window).height();
-  var windowW = $(window).width();
 
-  masthead.width(windowW);
   masthead.height(windowH);
 }
 
